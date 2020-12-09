7ca50d6e589e8db93cb17787a79404f935f0d2f6
flis_ann <ann.flismark@gmail.com>
2020-12-09 16:21:45 +0100

    adding shoulda_matcher file and adding FactoryBot and Shouldamatcha

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rails_helper'
 require 'rspec/rails'
 
-
 ActiveRecord::Migration.maintain_test_schema!
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
@@ -16,4 +15,6 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include FactoryBot::Syntax::Methods
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
 end
