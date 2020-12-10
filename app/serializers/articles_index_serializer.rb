6feeca1d3435137d2a07f95470d79f2cee8c0d3d
Ehugo2000 <erik.hgsn@gmail.com>
2020-12-10 11:47:28 +0100

    Adding test to see content in articles

--- app/serializers/articles_index_serializer.rb
+++ app/serializers/articles_index_serializer.rb
@@ -1,3 +1,7 @@
 class ArticlesIndexSerializer < ActiveModel::Serializer
-  attributes :id, :title, :lead, :body
+  attributes :id, :title, :lead, :body, :created
+
+  def created
+    object.created_at.strftime('%F')
+  end
 end
--- spec/requests/api/visitor_can_see_list_of_articles_spec.rb
+++ spec/requests/api/visitor_can_see_list_of_articles_spec.rb
@@ -8,6 +8,18 @@ RSpec.describe 'GET/api/articles' do
       expect(response).to have_http_status 200
     end
 
+    it 'is expected to have valid title in first "article"' do
+      expect(JSON.parse(response.body)['articles'][0]['title']).to eq 'MyTitle'
+    end
+
+    it 'is expected to have valid lead in second "article"' do
+      expect(JSON.parse(response.body)['articles'][1]['lead']).to eq 'MyLead'
+    end
+
+    it 'is expected to have valid body in third "article"' do
+      expect(JSON.parse(response.body)['articles'][2]['body']).to eq 'MyBody'
+    end
+
     it 'expected to return all articles' do
       expect(response_json['articles'].count).to eq 3
     end
