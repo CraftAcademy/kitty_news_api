b8f5b4c1729c45f32baaa6f0c6e875c56249a019
Sanne <sannelindholm@hotmail.com>
2021-01-05 19:54:20 +0100

    Adding validations to category spec in models folder

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,3 +1,19 @@
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'is expected to have db colums' do
+    it { is_expected.to have_db_column :label }
+  end
+
+  describe 'is expected to have validation' do
+    it { is_expected.to validates_presence_of :label }
+  end
+
+  describe 'is expected to have many products' do
+    it { is_expected.to have_many(:articles) }
+  end
+
+  describe 'Factory' do
+    it 'is expected to be valid' do
+      expect(create(:category)).to be_valid
+    end
+  end
 end
