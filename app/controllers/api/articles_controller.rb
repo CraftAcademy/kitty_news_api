0fd3a4bd4502b5d1272c30c0c9206cb6db2ed872
Ehugo2000 <erik.hgsn@gmail.com>
2021-01-07 16:43:16 +0100

    Updated articles_controller with a dynamic error message. Updating sad path test for unsuccessfull creating an article.

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -14,13 +14,12 @@ class Api::ArticlesController < ApplicationController
   def create
     article = Article.create(article_params)
     if article.persisted?
-      render json: { message: 'Your article was successfully created' }, status: 201
+      render json: { message: 'Your article was successfully created!' }, status: 201
     else
-      render json: { message: 'Something went wrong!' }, status: 422
+      render json: { message: article.errors.full_messages.to_sentence }, status: 422
     end
   end
 
-  
   private
 
   def article_params
