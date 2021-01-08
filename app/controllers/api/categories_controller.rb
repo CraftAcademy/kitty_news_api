9db7317211ddd3cd9c3341d747f2b8d97e2aa3c0
viktor <viktorkarlsson125@gmail.com>
2021-01-08 19:16:20 +0100

    remove id: from find(params['id']). modify visitor_can_view_article_by_category_spec

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,6 +1,6 @@
 class Api::CategoriesController < ApplicationController
   def show
-    category = Category.find(id: params['id'])
+    category = Category.find(params['id'])
     render json: category, serializer: CategoriesShowSerializer
   rescue StandardError => e
     render json: { message: 'Something went wrong, this category was not found' }, status: 404
