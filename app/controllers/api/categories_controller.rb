92f09231bf5c7c690c457bfeeb2adf4be61f6df2
viktor <viktorkarlsson125@gmail.com>
2021-01-08 16:39:39 +0100

    params to find category is changed from label to id

--- app/controllers/api/categories_controller.rb
+++ app/controllers/api/categories_controller.rb
@@ -1,6 +1,6 @@
 class Api::CategoriesController < ApplicationController
   def show
-    category = Category.find_by(label: params['id'])
+    category = Category.find_by(id: params['id'])
     render json: category, serializer: CategoriesShowSerializer
   rescue StandardError => e
     render json: { message: 'Something went wrong, this category was not found' }, status: 404
