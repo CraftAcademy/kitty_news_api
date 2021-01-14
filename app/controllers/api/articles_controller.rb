c1cadf23bb574d3900c0c9f8c54de90d877f830a
Sanne <sannelindholm@hotmail.com>
2021-01-14 14:14:32 +0100

    Installing active_storage and creating migration, adding has_one_attached image to article model, adding image mock method in test and creating attach image method in articles controller

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -16,7 +16,7 @@ class Api::ArticlesController < ApplicationController
 
   def create
     article = current_user.articles.create(article_params)
-    if article.persisted?
+    if article.persisted? && attach_image(article)
       render json: { message: 'Your article was successfully created!' }, status: 201
     else
       render json: { message: article.errors.full_messages.to_sentence }, status: 422
@@ -33,5 +33,12 @@ class Api::ArticlesController < ApplicationController
     unless current_user.journalist?
       render json: { message: 'You are not authorized to create an article.' }, status: 401
     end
+  end
+
+  def attach_image(article)
+    params_image = params[:article][:image]
+      if params_image.present?
+        DecodeService.attach_image(params_image, article.image)
+      end
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
   validates_presence_of :title, :lead, :body
   belongs_to :category
   belongs_to :author, class_name: 'User'
+  has_one_attached :image
 end
