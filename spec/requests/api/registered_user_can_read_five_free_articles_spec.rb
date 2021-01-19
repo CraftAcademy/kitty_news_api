d9299ccd7293ecc8dade7fab0579e0021670fa9a
Kyungin Na <nakyungin93@gmail.com>
2021-01-19 19:26:01 +0100

    Adding article_click to users table

--- spec/requests/api/registered_user_can_read_five_free_articles_spec.rb
+++ spec/requests/api/registered_user_can_read_five_free_articles_spec.rb
@@ -1,20 +1,25 @@
-RSpec.describe "GET /api/articles/:id", type: :request do
+RSpec.describe 'GET /api/articles/:id', type: :request do
   let(:registered_user) { create(:registered_user) }
   let(:registered_user_headers) { registered_user.create_new_auth_token }
-  let(:article) {create(:article)}
+  let(:article) { create(:article) }
 
-  describe "registered user can successfully" do
-    describe "read first free article" do
+  describe 'registered user can successfully' do
+    describe 'read first free article' do
       before do
-        get "/api/articles/#{article.id}"
+        get "/api/articles/#{article.id}",
+            headers: registered_user_headers
       end
 
-      it "is expected to return a 200 status" do
+      it 'is expected to return a 200 status' do
         expect(response).to have_http_status 200
       end
 
-      it "is expected to respond with the title" do
-        expect(response_json["article"]["title"]).to eq "MyTitle"
+      it 'is expected to respond with the title' do
+        expect(response_json['article']['title']).to eq 'MyTitle'
+      end
+
+      it 'expected to update articleClickOnUser to 1' do
+        expect(registered_user.reload.article_click).to eq 1
       end
     end
   end
